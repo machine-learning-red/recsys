8b1c16749f3ed3e9b71ed5395d2a5c0656d91e9e
cmelgarejo <cmelgarejo.dev@gmail.com>
2021-12-06 21:00:23 -0300

    apriori updates
    Signed-off-by: cmelgarejo <cmelgarejo.dev@gmail.com>

--- apriori_recsys.py
+++ apriori_recsys.py
@@ -2,12 +2,12 @@ import pandas as pd
 from apriori_python import apriori
 
 url = "./datasets/dataset_apriori.csv"
-df = pd.read_csv(filepath_or_buffer=url, names=range(100), dtype=str)
+df = pd.read_csv(filepath_or_buffer=url, sep=',', header=None, names=['col1', 'col2', 'col3'])
 
 print(df.head())
 # maybe turn this into a matrix manually with a loop loading with csv reader?
 
-freqItemSet, rules = apriori(df, minSup=0.5, minConf=0.5)
+freqItemSet, rules = apriori(df, minSup=0.11, minConf=0.5)
 print("ITEMSETS FRECUENTES:")
 print(freqItemSet)
 print("REGLAS DE ASOCIACION:")
